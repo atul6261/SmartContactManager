905a4a488eb5b2811e45c6113861f2669bea78c8
pankaj kumar pandit <pkumarpandit25@gmail.com>
2021-08-11 23:18:31 +0530

    first commit

---
+++ SmartContactManager/src/main/java/com/smart/helper/Helper.java
@@ -0,0 +1,28 @@
+package com.smart.helper;
+
+public class Helper {
+	
+	private String content;
+	private String type;
+	public Helper(String content, String type) {
+		super();
+		this.content = content;
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+
+}
